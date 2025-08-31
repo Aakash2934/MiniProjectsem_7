1a57f968cd8635d5e712bc368b38d6ebd0b9253e
Satish Singh <satishsinghss1910@gmail.com>
2025-08-31 08:21:40 +0530

    Data Ingest

--- scripts/ingest_data.py
+++ scripts/ingest_data.py
@@ -1,15 +1,13 @@
 import os
 from datasets import load_dataset, load_from_disk, get_dataset_split_names
-from tqdm.auto import tqdm
 
-dataset_name = "louisbrulenaudet/clinical-trials"
-output_folder = "../data"
-dataset_path = os.path.join(output_folder, "clinical_trials_dataset")
-if not os.path.exists(dataset_path):
-    os.makedirs(output_folder, exist_ok=True)
-    splits = get_dataset_split_names(dataset_name)
-    print("Available splits:", splits)
-    clinical_trials_dataset = load_dataset(dataset_name)
-    clinical_trials_dataset.save_to_disk(dataset_path)
-else:
-    clinical_trials_dataset = load_from_disk(dataset_path)
+def fetch_data(dataset_name: str, output_folder: str = "../data"):
+    dataset_path = os.path.join(output_folder, dataset_name.replace("/", "_"))
+    if not os.path.exists(dataset_path):
+        os.makedirs(output_folder, exist_ok=True)
+        dataset = load_dataset(dataset_name)
+        dataset.save_to_disk(dataset_path)
+    else:
+        dataset = load_from_disk(dataset_path)
+
+    return dataset
