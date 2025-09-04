90e511ee6d4613545eb3aa29ea169aabc4a66c45
Satish Singh <satishsinghss1910@gmail.com>
2025-09-04 14:10:08 +0530

    Created sample from train data to create annotation

--- scripts/sample.py
+++ scripts/sample.py
@@ -1,7 +1,37 @@
 import pandas as pd
+import os
 
-df = pd.read_csv("../data/processed/train.csv", usecols=["criterion_text"])
-df_sampled = df.sample(n=1000, random_state=42)
-df_sampled.to_csv("../data/processed/sample.txt", index=False, header=False)
+PROCESSED_DATA_FOLDER = os.path.join("data", "processed")
+PROCESSED_TRAIN_FILE = os.path.join(PROCESSED_DATA_FOLDER, "train.csv")
+ANNOTATION_SAMPLE_FILE = os.path.join(PROCESSED_DATA_FOLDER, "sample.txt")
+SAMPLE_SIZE = 1000
 
-print("Done")
+def create_sample_for_annotation():
+    if not os.path.exists(PROCESSED_TRAIN_FILE):
+        return
+
+    try:
+        df = pd.read_csv(PROCESSED_TRAIN_FILE)
+    except Exception as e:
+        print(f"Failed to load the CSV file. Reason: {e}")
+        return
+
+    if 'criterion_text' not in df.columns:
+        print("'criterion_text' column not found in the CSV.")
+        return
+
+    unique_criteria = df['criterion_text'].dropna().unique()
+    if len(unique_criteria) < SAMPLE_SIZE:
+        sample_size_to_take = len(unique_criteria)
+    else:
+        sample_size_to_take = SAMPLE_SIZE
+
+    sample_criteria = unique_criteria[:sample_size_to_take]
+    with open(ANNOTATION_SAMPLE_FILE, 'w', encoding='utf-8') as f:
+        for text in sample_criteria:
+            f.write(f"{text.strip()}\n")
+        
+        print(f"\nAnnotation sample file created successfully!")
+
+if __name__ == "__main__":
+    create_sample_for_annotation()
